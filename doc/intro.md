e92121e319444bd706dc3c6d2b3bd591bbe98aa2
Tanel Kriik <tanel.kriik@outlook.com>
2016-10-08 09:20:01 +0300

    Project stuff

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clopack-example
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/clopack_example/core_test.clj
@@ -0,0 +1,7 @@
+(ns clopack-example.core-test
+  (:require [clojure.test :refer :all]
+            [clopack-example.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
